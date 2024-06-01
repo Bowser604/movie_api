7eda4f36c5277d31ba8a7e273a50c16398b4ff05
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2024-05-31 18:12:48 -0700

    testing collection

--- index.js
+++ index.js
@@ -19,15 +19,19 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
 
-// mongoose.connect("mongodb+srv://Bowser604:Tiberius604@cluster0.9l61s4s.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=Cluster0", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-mongoose.connect( process.env.CONNECTION_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
+mongoose.connect("mongodb+srv://Bowser604:Tiberius604@cluster0.9l61s4s.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// mongoose.connect( process.env.CONNECTION_URI, { 
+//   useNewUrlParser: true, 
+//   useUnifiedTopology: true 
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((error) => {
+//   console.error('Error connecting to MongoDB:', error.message);
+// });
 
 const app = express();
 app.use(bodyParser.json());
