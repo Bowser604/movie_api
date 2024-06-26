ac65d87bc17ed3d2d2472c581ce49c10a674ce57
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2024-06-25 22:18:55 -0700

    connection_URI

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
 
-mongoose.connect("mongodb://localhost:27017/[Movies]", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// mongoose.connect( process.env.CONNECTION_URI, { 
-//   useNewUrlParser: true, 
-//   useUnifiedTopology: true 
-// }).then(() => {
-//   console.log('Connected to MongoDB');
-// }).catch((error) => {
-//   console.error('Error connecting to MongoDB:', error.message);
+// mongoose.connect("mongodb://localhost:27017/[Movies]", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
+
+mongoose.connect( process.env.CONNECTION_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
 const app = express();
 app.use(bodyParser.json());
