6ae2ce28149b74da7a346771575f27d6e2524fc2
Bowser604 <146279617+Bowser604@users.noreply.github.com>
2024-07-03 14:36:16 -0700

    updating

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8081', 'http://testsite.com'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
